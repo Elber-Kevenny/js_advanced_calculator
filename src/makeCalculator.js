d5f7a88d02b33e86df6412996941cdeb826287e4
Elber-Kevenny <vieirabeb@gmail.com>
2025-10-01 21:25:50 -0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,58 @@
 'use strict';
 
 /**
- * @return {object}
+ * // @return {object}
  */
 function makeCalculator() {
   // write code here
+
+  const add = (current, n) => {
+    return current + n;
+  };
+
+  const subtract = (current, n) => {
+    return current - n;
+  };
+
+  const divide = (current, n) => {
+    if (n === 0) {
+      return undefined;
+    } else {
+      return Math.ceil(current) / n;
+    }
+  };
+
+  const multiply = (current, n) => {
+    return current * n;
+  };
+
+  const reset = () => {
+    math.result = 0;
+
+    return math;
+  };
+
+  function operate(callback, n) {
+    const next = callback(math.result, n);
+
+    if (callback !== undefined) {
+      math.result = next;
+
+      return math;
+    }
+  }
+
+  const math = {
+    add,
+    subtract,
+    divide,
+    multiply,
+    reset,
+    operate,
+    result: 0,
+  };
+
+  return math;
 }
 
 module.exports = makeCalculator;
